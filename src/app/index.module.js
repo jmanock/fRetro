7f91563b1a288b265d50f8d2ec89a7095b56acda
jManock <jonmanock@gmail.com>
2018-04-16 12:04:16 -0400

    TheRealReDo

--- src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('fRetro', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr']);
-
-})();
