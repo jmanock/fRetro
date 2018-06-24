53714e22072cd4c39ae1f9c5cec94ad6970cbc09
jManock <jonmanock@gmail.com>
2018-06-24 03:25:02 -0400

    form works need a thank you page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,3 @@
-// function myMap(){
-//   var mapOptions = {
-//     center: new google.maps.LatLng(51.5, -0.12),
-//     zoom:10,
-//     mapTypeId:google.maps.MapTypeId.HYBRID
-//   }
-//   var map = new google.maps.Map(document.getElementById('map'),mapOptions);
-// }
-
 function myMap(){
   var uluru = {lat:28.6486639786372, lng:-81.3540162583253};
   var map = new google.maps.Map(document.getElementById('map'),{
@@ -18,3 +9,13 @@ function myMap(){
     map:map
   });
 }
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyYc6U2BcmWssKCfAH6emQovnWTrfUIpn8o1a9mGrZ5XAPAav4/exec';
+const form = document.forms['submit-to-google-sheet'];
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  fetch(scriptURL, {method:'POST', body: new FormData(form)})
+    .then(response => console.log('Success!', response))
+    .catch(error => console.error('Error!', error.message));
+});
