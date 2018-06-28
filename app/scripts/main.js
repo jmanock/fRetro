3169036336fb522fa170d44d5ea129e3047b55cc
jManock <jonmanock@gmail.com>
2018-06-28 15:44:43 -0400

    working on the scrolling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -25,3 +25,17 @@ function thankYou(){
     $('.thankYou').css({display:'block'});
   });
 }
+
+$(document).ready(function(){
+  $('a').on('click', function(e){
+    if(this.hash !== ''){
+      e.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash - 100;
+      });
+    }
+  });
+});
