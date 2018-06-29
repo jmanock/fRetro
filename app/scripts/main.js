c9033dfca2fe563f1d32d161f94b3a0c0fd1f627
jManock <jonmanock@gmail.com>
2018-06-29 12:07:30 -0400

    got min working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -27,19 +27,6 @@ function thankYou(){
 }
 
 $(document).ready(function(){
-  // $('a').on('click', function(e){
-  //   if(this.hash !== ''){
-  //     e.preventDefault();
-  //     var hash = this.hash;
-  //
-  //     $('html, body').animate({
-  //       scrollTop: $(hash).offset().top - 80
-  //     }, 800, function(){
-  //       window.location.hash = hash;
-  //     });
-  //   }
-  // });
-
   $('a').on('click', function(e){
     e.preventDefault();
     var target = this.hash;
@@ -47,7 +34,9 @@ $(document).ready(function(){
     $('html, body').stop().animate({
       'scrollTop':$target.offset().top - 85
     }, 900, 'swing', function(){
-      // window.location.hash = target;
-    })
-  })
+    });
+  });
 });
+
+// function myMap(){var uluru={lat:28.6486639786372,lng:-81.3540162583253};var map=new google.maps.Map(document.getElementById('map'),{zoom:16,center:uluru});var marker=new google.maps.Marker({position:uluru,map:map})}const scriptURL='https://script.google.com/macros/s/AKfycbyYc6U2BcmWssKCfAH6emQovnWTrfUIpn8o1a9mGrZ5XAPAav4/exec';const form=document.forms['submit-to-google-sheet'];form.addEventListener('submit',e=>{e.preventDefault();fetch(scriptURL,{method:'POST',body:new FormData(form)}).then(response=>console.log('Success!',response),thankYou()).catch(error=>console.error('Error!',error.message))});function thankYou(){$(document).ready(function(){$('form').hide();$('.thankYou').css({display:'block'})})}
+// $(document).ready(function(){$('a').on('click',function(e){e.preventDefault();var target=this.hash;var $target=$(target);$('html, body').stop().animate({'scrollTop':$target.offset().top-85},900,'swing',function(){})})})
